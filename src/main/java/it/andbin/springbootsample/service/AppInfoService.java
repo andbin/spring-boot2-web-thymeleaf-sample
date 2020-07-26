34e50d4fc133ae05bd94d9ba0657c37cc25cde00
andbin <andbin@users.noreply.github.com>
2020-07-26 17:58:27 +0200

    Add license comment

--- src/main/java/it/andbin/springbootsample/service/AppInfoService.java
+++ src/main/java/it/andbin/springbootsample/service/AppInfoService.java
@@ -1,3 +1,11 @@
+/*
+ * Copyright (C) 2019-2020 Andrea Binello ("andbin")
+ *
+ * This file is part of the "Spring Boot 2 Web Thymeleaf Sample" project
+ * and is released under the MIT License. See one of the license files
+ * included in the root of the project for the full text of the license.
+ */
+
 package it.andbin.springbootsample.service;
 
 import java.lang.management.ManagementFactory;
